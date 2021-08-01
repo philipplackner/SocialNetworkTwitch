a9d866dc62fd42c763736eb0962556500824724d
Philipp <mail@pl-coding.com>
2021-08-01 17:25:45 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/socialnetworktwitch/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.plcoding.socialnetworktwitch.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF404040)
+val LightGray = Color(0xFF606060)
+val TextWhite = Color(0xFFEEEEEE)
+val HintGray = Color(0xFF6D6D6D)
+val GreenAccent = Color(0xFF08FF04)
---
+++ app/src/main/java/com/plcoding/socialnetworktwitch/ui/theme/Theme.kt
@@ -0,0 +1,26 @@
+package com.plcoding.socialnetworktwitch.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = GreenAccent,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray,
+    surface = MediumGray,
+    onSurface = LightGray
+)
+
+@Composable
+fun SocialNetworkTwitchTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
