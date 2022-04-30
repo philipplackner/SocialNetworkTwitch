503a53afe11da7aedfb6ca96e7783b8feac47198
Johannes Bester <bester.jpb@gmail.com>
2022-04-30 17:02:07 +0200

    Added Image Cropper

--- app/src/main/java/com/plcoding/socialnetworktwitch/presentation/create_post/CreatePostScreen.kt
+++ app/src/main/java/com/plcoding/socialnetworktwitch/presentation/create_post/CreatePostScreen.kt
@@ -1,5 +1,9 @@
 package com.plcoding.socialnetworktwitch.presentation.create_post
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,20 +13,26 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
 import com.plcoding.socialnetworktwitch.R
 import com.plcoding.socialnetworktwitch.presentation.components.StandardTextField
 import com.plcoding.socialnetworktwitch.presentation.components.StandardToolbar
 import com.plcoding.socialnetworktwitch.presentation.ui.theme.SpaceLarge
 import com.plcoding.socialnetworktwitch.presentation.ui.theme.SpaceMedium
 import com.plcoding.socialnetworktwitch.presentation.ui.theme.SpaceSmall
+import com.plcoding.socialnetworktwitch.presentation.util.loadPicture
 import com.plcoding.socialnetworktwitch.presentation.util.states.StandardTextFieldState
 
 @Composable
@@ -30,6 +40,27 @@ fun CreatePostScreen(
     navController: NavController,
     viewModel: CreatePostViewModel = hiltViewModel()
 ) {
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    
+    val imageCropLauncher = rememberLauncherForActivityResult(CropImageContract()) { result ->
+        if (result.isSuccessful) {
+            // use the cropped image
+            imageUri = result.uriContent
+        } else {
+            // an error occurred cropping
+            val exception = result.error
+        }
+    }
+    
+    val imagePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        val cropOptions = CropImageContractOptions(uri, CropImageOptions())
+        imageCropLauncher.launch(cropOptions)
+    }
+    
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -59,15 +90,27 @@ fun CreatePostScreen(
                         shape = MaterialTheme.shapes.medium
                     )
                     .clickable {
-
+                        imagePickerLauncher.launch("image/*")
                     },
                 contentAlignment = Alignment.Center
             ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = stringResource(id = R.string.choose_image),
-                    tint = MaterialTheme.colors.onBackground
-                )
+                if (imageUri != null) {
+                    val bitmapImage = loadPicture(url = imageUri.toString(), defaultImage = R.drawable.channelart).value
+                    bitmapImage?.let { bitmap ->
+                        Image(
+                            bitmap = bitmap.asImageBitmap(),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                } else {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = stringResource(id = R.string.choose_image),
+                        tint = MaterialTheme.colors.onBackground
+                    )
+                }
             }
             Spacer(modifier = Modifier.height(SpaceMedium))
             StandardTextField(
